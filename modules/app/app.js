c9340972435efa1da36eafcec8f0a4610cecd98e
Ming Cheng <cmgrace@bu.edu>
2021-02-24 11:25:13 -0500

    deploy

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const { CLIENT_ORIGIN } = require("../../config");
+const { CLIENT_ORIGIN } = require("../../config");
 
 const app = express();
 
@@ -15,5 +15,16 @@ app.use("/api", require("../people/people.router"));
 app.use("/api", require("../pets/pets.router"));
 app.use("/api", require("../cats/cats.router"));
 app.use("/api", require("../dogs/dogs.router"));
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  console.error(error);
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
 module.exports = app;
