7841094bfc646204b7ec7779b4af656b8d394033
Ming Cheng <cmgrace@bu.edu>
2021-02-23 23:23:31 -0500

    pre-deploy

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const { CLIENT_ORIGIN } = require("../../config");
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 
 app.use("/api", require("../people/people.router"));
 app.use("/api", require("../pets/pets.router"));
